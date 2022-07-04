412feb712edc60614d9bbb6955e5f55bf0eb31ec
lolo23333 <1239208285@qq.com>
2022-07-04 17:28:06 +0800

    The first time

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+
+    private static final  int a=1;
+    private static final  int b=2;
+
+    public static void main(String[] args) {
+        System.out.println(a+b);
+    }
+}
