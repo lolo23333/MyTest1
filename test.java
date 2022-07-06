718c302ed0cdffdcca30357e8c725d975bf9c162
lolo23333 <1239208285@qq.com>
2022-07-06 09:08:47 +0800

    The second time

--- test.java
+++ test.java
@@ -1,9 +1,8 @@
 public class test {
 
-    private static final  int a=1;
-    private static final  int b=2;
+
 
     public static void main(String[] args) {
-        System.out.println(a+b);
+        System.out.println("one plus one is two");
     }
 }
